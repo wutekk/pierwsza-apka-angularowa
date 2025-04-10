8c51bf7003a02cf7287237b6bea6c9dd82001d98
wutekk <wutek13@gmail.com>
2025-04-10 20:09:49 +0200

    lepsza lista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
+import { LISTA_ZAKUPOW } from './items.data';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Lista zakupów do zrobienia';
+  lista_zakopow = LISTA_ZAKUPOW;
 }
---
+++ src/app/items.data.ts
@@ -0,0 +1,12 @@
+export const LISTA_ZAKUPOW: string[] = [
+    'Mleko',
+    'Chleb',
+    'Masło',
+    'Jajka',
+    'Ser',
+    'Pomidory',
+    'Ogórki',
+    'Ziemniaki',
+    'Marchew',
+    'Cebula'
+]

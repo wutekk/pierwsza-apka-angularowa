5f47045dfaa2a9387269cd330f89bcb68863aea2
wutekk <wutek13@gmail.com>
2025-04-10 19:11:59 +0200

    init angulara

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'default';
+}

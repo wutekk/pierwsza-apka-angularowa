44af983658a5b7419349667e3cfc6189df6cda94
wutekk <wutek13@gmail.com>
2025-05-08 19:31:49 +0200

    Przycisk dodawania_zakupuw
    Co-authored-by: mteblesz <mteblesz@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,21 @@ import { Component, signal, WritableSignal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StorageService } from './storage.service';
 import { LISTA_ZAKUPOW_NAZWY } from './items.data';
-import { Rzecz } from './ app.service';
+import { Rzecz } from './types';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Lista zakupów do zrobienia';
   lista_zakupow;
+  nowej_rzeczy_nazwa: string = '';
 
   constructor(
     private storageService: StorageService
@@ -60,5 +62,24 @@ export class AppComponent {
     this.lista_zakupow.set(lista_pom);
     this.storageService.save(this.lista_zakupow())
   }
-  
+
+  dodaj_zakup(): void {
+    this.nowej_rzeczy_nazwa = this.nowej_rzeczy_nazwa.trim();
+    if (this.nowej_rzeczy_nazwa.length == 0) {
+      return
+    }
+    const lista = this.lista_zakupow();
+
+    const nowa_rzecz: Rzecz = {
+      nazwa: this.nowej_rzeczy_nazwa,
+      zaznaczenie: false
+    }
+    lista.push(nowa_rzecz)
+
+    this.lista_zakupow.set(lista);
+    this.storageService.save(this.lista_zakupow())
+
+    this.nowej_rzeczy_nazwa = ''
+  }
 }
+

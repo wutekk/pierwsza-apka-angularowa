43a9e55814d87ecb15d0a0cb50d200ea38020fd2
mteblesz <mteblesz@gmail.com>
2025-05-08 20:38:16 +0200

    powrot do naszych funckji tylko ze sa w serwisie

--- src/app/components/formularz_dodawania/folmularz-dodawania/folmularz-dodawania.component.ts
+++ src/app/components/formularz_dodawania/folmularz-dodawania/folmularz-dodawania.component.ts
@@ -9,24 +9,5 @@ import { Component } from '@angular/core';
 })
 export class FolmularzDodawaniaComponent {
 
-  dodaj_zakup(): void {
-    this.nowej_rzeczy_nazwa = this.nowej_rzeczy_nazwa.trim();
-    if (this.nowej_rzeczy_nazwa.length == 0) {
-      return
-    }
-    const lista = this.lista_zakupow();
-
-    const nowa_rzecz: Rzecz = {
-      nazwa: this.nowej_rzeczy_nazwa,
-      zaznaczenie: false
-    }
-    lista.push(nowa_rzecz)
-
-    this.lista_zakupow.set(lista);
-    this.storageService.save(this.lista_zakupow())
-
-    this.input_pokazany = false;
-    this.nowej_rzeczy_nazwa = '';
-  }
-
+// TODO
 }
